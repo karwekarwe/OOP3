d41aa788c0fa1bced29d7943ee8341762f227b59
Dominykas Mykolaitis <domasmyko@gmail,com>
2024-05-16 19:00:51 +0300

    dok

---
+++ dokumentacija/html/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['v2_200_0',['v2.0',['../md__r_e_a_d_m_e.html#autotoc_md1',1,'']]],
+  ['veikimas_3a_1',['Programos veikimas:',['../md__r_e_a_d_m_e.html#autotoc_md3',1,'']]]
+];
